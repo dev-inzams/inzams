7b6a297ee0009131dcce525c876489bd8e77c6a6
inzams <inzams.hp@gmail.com>
2023-12-12 08:22:33 +0600

    secomd commit

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -7,7 +7,26 @@
     <div class="flex">
 
         <div class="flex-1">
-            inzams
+            <div class="hello">
+                <h3>Hello, I'm</h3>
+            </div>
+            <div class="name">
+                <h2>Inzams</h2>
+            </div>
+            <div class="title">
+                A <span>Laravel</span> web developer from <span>Bangladesh</span>
+            </div>
+            <div class="details">
+                <p>I'm creative designer based in New York, and I'm very passionate and dedicated to my work.</p>
+            </div>
+            <div class="flex">
+                <div class="flex-1">
+
+                </div>
+                <div class="flex-2">
+
+                </div>
+            </div>
         </div>
 
 
